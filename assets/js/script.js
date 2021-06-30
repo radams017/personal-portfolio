b991fa807176ae14e114f7dc496f6cc893e243fb
Ryan Adams <ryanadams14@gmail.com>
2021-06-29 23:47:32 -0500

    added projects, updated bootstrap, added javascript for button handlers, started contact icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
-import typer from "./typer";
+// button handlers
 
-$(function () {
-    typer('.typewriter').line("I Am A Developer")
-});
+$('#cors-deployed').click(() => {
+    window.open('https://radams017.github.io/chrome-referrer-compliance-tool/', '_blank')
+})
+
+$('#cors-repo').click(() => {
+    window.open('https://github.com/radams017/chrome-referrer-compliance-tool/', '_blank')
+})
+
+$('#crypto-deployed').click(() => {
+    window.open('https://radams017.github.io/Defi-101/', '_blank')
+})
+
+$('#crypto-repo').click(() => {
+    window.open('https://github.com/radams017/Defi-101', '_blank')
+})
+
+$('#plan-deployed').click(() => {
+    window.open('https://radams017.github.io/day-planner/', '_blank')
+})
+
+$('#plan-repo').click(() => {
+    window.open('https://github.com/radams017/day-planner', '_blank')
+})
+
+$('#quiz-deployed').click(() => {
+    window.open('https://radams017.github.io/code-quiz/', '_blank')
+})
+
+$('#quiz-repo').click(() => {
+    window.open('https://github.com/radams017/code-quiz', '_blank')
+})
